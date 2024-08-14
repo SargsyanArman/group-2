6ced8320f59e1fe122b6d564e1a32bdb358c19c1
Levon Sargsyan <levon.sargsyan2@tumo.org>
2024-08-14 21:38:07 +0400

    Better css

--- src/Components/ItemsList/Card.jsx
+++ src/Components/ItemsList/Card.jsx
@@ -3,8 +3,7 @@ const Card = ({ itemURL, imgSrc, price, currency, title, category, rating, rNum 
         <div className="card">
             <a href={itemURL}>
                 <span>
-                    <div className="thumbnail">
-                        <img src={imgSrc} alt="" />
+                    <div className="thumbnail" style={{background: `center/100% no-repeat url(${imgSrc})`}}>
                         <button className="likeButton"><i className="fa-regular fa-heart"></i></button>
                         <div className="tags">
 
@@ -13,8 +12,9 @@ const Card = ({ itemURL, imgSrc, price, currency, title, category, rating, rNum 
                     <div className="info">
                         <h4 className="price">{price} {currency}</h4>
                         <p className="itemTitle">{title} <span className="categories">/ {category}</span></p>
-                        <div className="rating"> {rating} <span className="ratingsNumber"> &#8901; {rNum} </span></div>
+                        <span className="rating"><i className="fa-solid fa-star"></i> {rating} </span><span className="ratingsNumber"> &#8901; {rNum} votes</span>
                     </div>
+                    <button className="cartBtn"><i className="fa-solid fa-cart-shopping"></i> Add to cart</button>
                 </span>
             </a>
         </div>
--- src/Components/ItemsList/List.jsx
+++ src/Components/ItemsList/List.jsx
@@ -2,23 +2,34 @@ import Card from "./Card";
 import './ItemsList.css';
 
 const List = () => {
+    const cardExample = {
+        url: '',
+        imgSrc: 'imageExample.webp',
+        price: 20,
+        currency: '$',
+        title: 'asd',
+        category: 'dsa',
+        rating: 4.9,
+        rNum: 1000
+    };
+    const cardsArr = []
+    for (let i = 0; i < 50; i++) {
+        cardsArr.push(<li key={i}>
+            <Card
+                itemURL={cardExample.url}
+                imgSrc={cardExample.imgSrc}
+                price={cardExample.price}
+                currency={cardExample.currency}
+                title={cardExample.title}
+                category={cardExample.category}
+                rating={cardExample.rating}
+                rNum={cardExample.rNum}
+            ></Card></li>);
+    }
+
     return (
         <ul className="list">
-            <li>
-                <Card itemURL='' imgSrc='' price={20} currency='$' title='asd' category='dsa' rating={4.5} rNum={100} />
-            </li>
-            <li>
-                <Card itemURL='' imgSrc='' price={20} currency='$' title='asd' category='dsa' rating={4.5} rNum={100} />
-            </li>
-            <li>
-                <Card itemURL='' imgSrc='' price={20} currency='$' title='asd' category='dsa' rating={4.5} rNum={100} />
-            </li>
-            <li>
-                <Card itemURL='' imgSrc='' price={20} currency='$' title='asd' category='dsa' rating={4.5} rNum={100} />
-            </li>
-            <li>
-                <Card itemURL='' imgSrc='' price={20} currency='$' title='asd' category='dsa' rating={4.5} rNum={100} />
-            </li>
+            {cardsArr}
         </ul>
     );
 };
