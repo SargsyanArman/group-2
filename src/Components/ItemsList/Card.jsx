ac91856835dc178aaf34a9e893bb1d6326a907c6
Arman <11arman@bk.ru>
2024-08-14 07:26:01 -0800

    A littile i've worked with cards

--- src/Components/ItemsList/Card.jsx
+++ src/Components/ItemsList/Card.jsx
@@ -1,25 +1,33 @@
+import React from 'react';
+import './ItemsList.css';
+
 const Card = ({ itemURL, imgSrc, price, currency, title, category, rating, rNum }) => {
     return (
         <div className="card">
-            <a href={itemURL}>
-                <span>
-                    <div className="thumbnail">
-                        <img src={imgSrc} alt="" />
-                        <button className="likeButton"><i className="fa-regular fa-heart"></i></button>
-                        <div className="tags">
-
-                        </div>
+            <a href={itemURL} className="card-link">
+                <div className="thumbnail">
+                    <img src={imgSrc} alt={title} className="thumbnail-image" />
+                    <button className="like-button">
+                        <i className="fa-regular fa-heart"></i>
+                    </button>
+                    <div className="tags">
+                        {/* Tags can be added here */}
                     </div>
-                    <div className="info">
-                        <h4 className="price">{price} {currency}</h4>
-                        <p className="itemTitle">{title} <span className="categories">/ {category}</span></p>
-                        <div className="rating"> {rating} <span className="ratingsNumber"> &#8901; {rNum} </span></div>
+                </div>
+                <div className="info">
+                    <h4 className="price">{price} {currency}</h4>
+                    <p className="item-title">
+                        {title}
+                        <span className="categories">/ {category}</span>
+                    </p>
+                    <div className="rating">
+                        {rating}
+                        <span className="ratings-number"> &#8901; {rNum}</span>
                     </div>
-                </span>
+                </div>
             </a>
         </div>
     );
 };
 
 export default Card;
-
--- src/Components/ItemsList/List.jsx
+++ src/Components/ItemsList/List.jsx
@@ -5,7 +5,7 @@ const List = () => {
     return (
         <ul className="list">
             <li>
-                <Card itemURL='' imgSrc='' price={20} currency='$' title='asd' category='dsa' rating={4.5} rNum={100} />
+                <Card itemURL='' imgSrc='https://media.istockphoto.com/id/2065674519/photo/rolling-says-macro.webp?s=2048x2048&w=is&k=20&c=FO-u3p_njEoIh7GusFYgrOo1RxF0EXobx0BXH6vMB4Q=' price={20} currency='$' title='asd' category='dsa' rating={4.5} rNum={100} />
             </li>
             <li>
                 <Card itemURL='' imgSrc='' price={20} currency='$' title='asd' category='dsa' rating={4.5} rNum={100} />
